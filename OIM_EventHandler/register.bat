668fca7075479bb26e458fa48777806e71c2b46f
Kenneth Reistad <kenr@steria.no>
2013-02-14 11:08:03 +0100

    Path-change

--- OIM_EventHandler/register.bat
+++ OIM_EventHandler/register.bat
@@ -1,5 +1,5 @@
 "c:\Program Files\Java\jdk1.7.0_13\bin\jar.exe" cvf deploy\lib\UsernamePolicy.jar -C bin .
-cd c:\oracle\deploy
+cd deploy
 "C:\Program Files\7-Zip\7z.exe" a ..\UsernamePolicyPlugin.zip .
-cd c:\oracle
+cd ..
 "c:\Program Files\Java\jdk1.7.0_13\bin\java.exe" -cp bin;lib\wlfullclient.jar;lib\oimclient.jar;lib\spring.jar;lib\log4j-1.2.8.jar;lib\commons-logging.jar no.steria.tad.PluginDeploy register UsernamePolicyPlugin.zip
